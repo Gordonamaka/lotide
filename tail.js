a931e29761e74a4f148bcae3227a56c987147e94
Amobi Gordon Maka <amobimak@gmail.com>
2021-09-14 01:00:45 +0000

    tail finished I hope

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+  
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+  if (actual === expected) {
+  
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+
+};
+
+const tail = function(array) {
+  return array.slice[1];
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length,3);
+
