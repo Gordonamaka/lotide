b3c0dcef48e73e6008d6004b70bc974910b1afbc
Amobi Gordon Maka <amobimak@gmail.com>
2021-09-15 00:33:25 +0000

    finished

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length >= array2.length) {     
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]){
+      return false;
+    }
+  }
+  return true;
+} else if (array1.length < array2.length) {
+  for (let i = 0; i < array2.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+  } 
+};
+    
+
+    
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
