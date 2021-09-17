e5a1ea968b24a402d1e38251c2720e257edad6f5
Amobi Gordon Maka <amobimak@gmail.com>
2021-09-17 09:32:20 +0000

    finished letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,58 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length >= array2.length) {     
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]){
+      return false;
+    }
+  }
+  return true;
+} else if (array1.length < array2.length) {
+  for (let i = 0; i < array2.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+  } 
+};
+
+const assertArrayEqual = function (array1,array2) {
+  if (!eqArrays(array1,array2)) {
+  
+    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+  }
+  else if (eqArrays(array1,array2)) {
+  
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  }
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  
+  
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    
+    if (sentence[i] !== " ") { 
+      
+      if (results[sentence[i]]) {
+
+        results[sentence[i]].push(i);
+
+      } else {
+
+        results[sentence[i]] = [i];
+      }
+
+    } return results;
+};
+
+
+
+assertArraysEqual(letterPositions("Cello").C, [0]);
+assertArraysEqual(letterPositions("Cello").e, [1]);
+assertArraysEqual(letterPositions("Cello").l, [2, 3]);
+assertArraysEqual(letterPositions("Cello").l, [2, 3]);
+assertArraysEqual(letterPositions("Cello").o, [4]);
