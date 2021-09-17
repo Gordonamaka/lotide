bcf678c7c7f38adde53406074a07083a86035aa3
Amobi Gordon Maka <amobimak@gmail.com>
2021-09-17 14:54:22 +0000

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,31 +1,24 @@
 const eqArrays = function(array1, array2) {
+
   if (array1.length >= array2.length) {     
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]){
-      console.log(false);
+      return false;
     }
   }
   return true;
+
 } else if (array1.length < array2.length) {
   for (let i = 0; i < array2.length; i++) {
     if (array1[i] !== array2[i]) {
-      console.log(false);
+      return false;
+      }
     }
-  }
-  return true;
+    return true;
   } 
 };
+
+const assertEqual = require('./assertEqual');
+
+module.exports = eqArrays;
     
-
-    
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
