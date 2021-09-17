6bf8b92ff86f27fcb53854ec3fc961380cc5a67f
Amobi Gordon Maka <amobimak@gmail.com>
2021-09-17 09:22:50 +0000

    countletters fin

---
+++ countLetters.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+  
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+  if (actual === expected) {
+  
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+
+};
+
+//The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence. 
+// I assume we'll need .length to solve, but OF EACH letter.
+const countLetters = function(allLetters) {
+  const countString = allLetters.split(" ").join("");
+  let results = {};
+  for (let lt of countString) {
+    if (results[lt]) {
+      results[lt] += 1;
+    } else {
+      results[lt] = 1;
+    }
+  }
+  return results;
+};
+ // We need two parameters, one to examine and display the letters, and the other to count the letters
+
+// variable for our result & our count for lt parameter
+// parameter execution (for loop to iterate over a string) for letters to push the letter into object
+
+
+//present lettersKey as an object key 
+
+
+// 
+
+// create a count variable (lt) to be returned upon counting strings
+
+
+console.log(countLetters("Lighthouse is pain I can tolerate"))
