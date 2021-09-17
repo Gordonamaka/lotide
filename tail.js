9c6d7322efa070d98e04e7f9d7cd774b6d0db881
Amobi Gordon Maka <amobimak@gmail.com>
2021-09-17 14:31:58 +0000

    tail refactored

--- tail.js
+++ tail.js
@@ -1,20 +1,21 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-  
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-  if (actual === expected) {
-  
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
-  return array.slice[1];
+  let hd = [];
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (i !== 0) {
+      hd.push(array[i]);
+    }
+  }
+  return hd;
 };
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
+console.log(tail(words));
 assertEqual(words.length,3);
+
+
+module.exports = tail;
 
