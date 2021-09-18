6864a8907748e31ed3cabab509fe52e2ed61ebac
Amobi Gordon Maka <amobimak@gmail.com>
2021-09-18 03:28:44 +0000

    completed index

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const assertArrayEqual = require('./assertArraysEqual')
-const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
+const assertArrayEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const countLetters = require('/countLetters');
+const countOnly = require('/countOnly');
+const map = require('/map');
+const takeUntil = require('/takeUntil');
+const without = require('/without');
+
+
 
 
 
@@ -13,5 +25,15 @@ module.exports = {
   middle: middle,
   eqArrays: eqArrays,
   assertArrayEqual: assertArrayEqual,
-  assertEqual: assertEqual
+  assertObjectsEqual: assertObjectsEqual,
+  assertEqual: assertEqual,
+  letterPositions: letterPositions,
+  findKey : findKey,
+  findKeyByValue: findKeyByValue,
+  eqObjects: eqObjects,
+  map: map,
+  takeUntil: takeUntil,
+  without: without,
+  countOnly: countOnly,
+  countLetters: countLetters,
 };
