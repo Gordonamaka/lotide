5b798cd642a6fb7cdcbcca5d422ff74e0268a088
Amobi Gordon Maka <amobimak@gmail.com>
2021-09-18 00:25:21 +0000

    map exercise

---
+++ map.js
@@ -0,0 +1,25 @@
+const eqArrays = require('./eqArrays');
+
+const assertEqual = require('./assertEqual');
+
+const assertArrayEqual = function (array1,array2) {
+  if (!eqArrays(array1,array2)) {
+  
+    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+  }
+  else if (eqArrays(array1,array2)) {
+  
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  }
+};
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+
