cdda8cd24370a3141004625c5d198ea1ec05e275
Amobi Gordon Maka <amobimak@gmail.com>
2021-09-18 03:20:32 +0000

    fixed spelling error

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+  
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+  if (actual === expected) {
+  
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+
+};
+
+const findKeyByValue = function(object, value) { 
+     
+  let arrayOfKeys = Object.keys(object); 
+     
+    for (let keys of arrayOfKeys) {
+
+       if (object[keys] === value) {
+
+             return keys; 
+       }
+     }
+}
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+module.exports = findKeyByValue;
